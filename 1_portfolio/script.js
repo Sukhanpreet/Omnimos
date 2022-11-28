d2aed13ecce6168dda30dd9647309e59693ab86e
Sukhanpreet <sukhanpreet356@gmail.com>
2022-11-28 23:34:19 +0530

    section updates

--- 1_portfolio/script.js
+++ 1_portfolio/script.js
@@ -2,9 +2,40 @@ console.log("script working");
 
 // let menu=document.querySelector('.menu');
 
-let menu=document.getElementById('menu')
+let menu=document.getElementById('menu');
 let navLinks=document.querySelector('.nav-links');
 
 menu.addEventListener('click',()=>{
     navLinks.classList.toggle('show');
 });
+
+
+// let overlay = document.querySelectorAll('.pro');
+
+// console.log(overlay);
+
+// for(let i=0;i<overlay.length;i++){
+//     overlay[i].addEventListener('hover',()=>{
+//         overlay[i].classList.toggle('pro-show');
+//     })
+// }
+
+let inputs=document.querySelectorAll('form input');
+
+let textarea=document.querySelector('textarea');
+
+let inputArr=[...inputs];
+
+inputArr.push(textarea);
+
+
+for(let i=0;i<inputArr.length;i++){
+    inputArr[i].addEventListener('focus',function(){
+        this.parentElement.classList.add('label-sm');
+    });
+    inputArr[i].addEventListener('blur',function(){
+        this.parentElement.classList.remove('label-sm');
+    });
+
+
+}
